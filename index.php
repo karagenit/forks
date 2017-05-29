03daad70cacd6f5b1e0509a5feb43c29d3e865a9
Caleb Smith <karagenit@outlook.com>
2017-05-29 19:59:24 +0000

    Make Curl Functional

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-    $curl = curl_init("https://api.github.com/repos/mangini/gdocs2md/forks");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, "karagenit");
-    $data = curl_exec($curl);
-    curl_close($curl);
 
-    $fork_arr = json_decode($data);
+    function get_curl($url) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, "karagenit");
+        $data = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($data);
+    }
 
+    $fork_arr = get_curl("https://api.github.com/repos/mangini/gdocs2md/forks");
+    
     //var_dump($fork_arr[0]);
     
     foreach($fork_arr as $index => $fork) {
