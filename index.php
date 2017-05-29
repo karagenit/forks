6e4975896344846d6a45f2d3f574352378091fd2
Caleb Smith <karagenit@outlook.com>
2017-05-29 17:03:40 +0000

    Add User Agent to Query

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
     $curl = curl_init("https://api.github.com/repos/mangini/gdocs2md/forks");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, "karagenit");
+    //curl_setopt($curl, CURLINFO_HEADER_OUT, true);
     $data = curl_exec($curl);
+    //$info = curl_getinfo($curl);
     curl_close($curl);
     echo $data;
 ?>
