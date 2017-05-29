55f91622222e3377a9fbcc5bc85a14a565549749
Caleb Smith <karagenit@outlook.com>
2017-05-29 19:21:09 +0000

    Access JSON Object Members

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
     $curl = curl_init("https://api.github.com/repos/mangini/gdocs2md/forks");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_USERAGENT, "karagenit");
-    //curl_setopt($curl, CURLINFO_HEADER_OUT, true);
     $data = curl_exec($curl);
-    //$info = curl_getinfo($curl);
     curl_close($curl);
-    echo $data;
+
+    $arr = json_decode($data);
+
+    //var_dump($arr[0]);
+
+    echo $arr[0]->id;
 ?>
