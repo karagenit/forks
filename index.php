d6e05273e862925eb1bce865b0b1f000a02cd0df
Caleb Smith <karagenit@outlook.com>
2017-06-11 03:46:09 +0000

    Use GraphQL API
    Doesn't currently work: returns "Problems Parsing JSON" error.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-    function get_curl($url) {
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: token ".file_get_contents("oauth.token")));
+    function get_curl($json) {
+        $curl = curl_init("https://api.github.com/graphql");
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Length: ".strlen($json), "Authorization: token ".file_get_contents("oauth.token")));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_USERAGENT, "RandomAgent");
-        //curl_setopt($curl, CURLOPT_USERPWD, "user:pass"); //despite this working with bash cURL, can't just pass the token here
-        //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         $data = curl_exec($curl);
         curl_close($curl);
         return json_decode($data);
     }
    
-    echo var_dump(get_curl("https://api.github.com/rate_limit"));
+    echo var_dump(get_curl('{"query": "query { rateLimit { remaining }}"}'));
 ?>
