8024704d95599cfc7fc2c1b60374e5084af39824
Caleb Smith <karagenit@outlook.com>
2017-06-11 03:53:56 +0000

    Fix GraphQL Query JSON Syntax

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         $curl = curl_init("https://api.github.com/graphql");
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Length: ".strlen($json), "Authorization: token ".file_get_contents("oauth.token")));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Length: ".strlen($json), "Authorization: bearer ".file_get_contents("oauth.token")));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_USERAGENT, "RandomAgent");
         $data = curl_exec($curl);
@@ -12,5 +12,5 @@
         return json_decode($data);
     }
    
-    echo var_dump(get_curl('{"query": "query { rateLimit { remaining }}"}'));
+    echo var_dump(get_curl("{\n\"query\": \"query { rateLimit { limit }}\"\n}"));
 ?>
