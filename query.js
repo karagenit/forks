ff51a4875f86ccbc6c3162e2293421767e69cc4f
Caleb Smith <karagenit@outlook.com>
2017-06-12 02:03:51 +0000

    Reduce Query Count for Testing

--- query.js
+++ query.js
@@ -1,6 +1,6 @@
 query($owner:String! $name:String!){
     repository(owner: $owner name: $name) {
-        forks(first:100 orderBy:{field:PUSHED_AT, direction:DESC}) {
+        forks(first:5 orderBy:{field:PUSHED_AT, direction:DESC}) {
             edges {
                 node {
                     nameWithOwner
