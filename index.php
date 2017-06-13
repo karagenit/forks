90dbfbe1b80b5e625b3be98a05e56c1cdb19adad
Caleb Smith <karagenit@outlook.com>
2017-06-13 00:17:26 +0000

    Use Input as Query Variables

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 
 <form action="result.php" method="post">
 Owner:<br>
-<input type="text" name="rowner"><br>
+<input type="text" name="owner"><br>
 Name:<br>
-<input type="text" name="rname"><br>
+<input type="text" name="name"><br>
 <input type="submit" value="Lookup">
 </form>
 
--- result.php
+++ result.php
@@ -32,14 +32,15 @@
         $json = $json."\n}";    
         return $json;
     }
-
-    $json = build_curl(file_get_contents("query.js"), file_get_contents("variables.js"));
+    $vars = json_encode(array("owner"=>$_POST['owner'], "name"=>$_POST['name']));
+    $json = build_curl(file_get_contents("query.js"), $vars);
     $forks = json_decode(get_curl($json));
     $sorted_forks = array();
 
     echo "<pre>";
     //echo $json."\n";
     //echo var_dump(get_curl($json));
+    //echo var_dump($_POST);
     foreach($forks->data->repository->forks->edges as $fork) {
         $sorted_forks[$fork->node->nameWithOwner] = $fork->node->watchers->totalCount;
     }
