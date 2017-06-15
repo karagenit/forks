5e3f5c79c9351c8819359b5e193cd494e1f38b62
Caleb Smith <karagenit@outlook.com>
2017-06-15 20:38:19 +0000

    Simplify Index
    See #20

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?php
-    session_start();
-    $client_id = file_get_contents("client_id.token");
-?>
-
 <!DOCTYPE html>
 <html>
 <body>
     <a href="query.php">Search Repos</a><br>
-    <a href="https://github.com/login/oauth/authorize?client_id=<?php echo $client_id; ?>">Authorize via Github</a>
+    <a href="auth.php">Authorize via Github</a>
 </body>
 </html>
