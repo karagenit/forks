7c6085d2743a36b60ae6533f5c82a0fbbd61973d
Caleb Smith <karagenit@outlook.com>
2017-06-15 19:17:00 +0000

    Create New Index Page

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<form action="result.php" method="post">
-Owner:<br>
-<input type="text" name="owner"><br>
-Name:<br>
-<input type="text" name="name"><br>
-<input type="submit" value="Lookup">
-</form>
-
+    <a href="query.php">Search Repos</a>
 </body>
 </html>
---
+++ query.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="result.php" method="post">
+Owner:<br>
+<input type="text" name="owner"><br>
+Name:<br>
+<input type="text" name="name"><br>
+<input type="submit" value="Lookup">
+</form>
+
+</body>
+</html>
