602cfd617cc65667c2e379743a9186bd45ab98cb
Caleb Smith <karagenit@outlook.com>
2017-06-15 20:18:45 +0000

    Make get_curl Use Given Token

--- curl_graphql_lib.php
+++ curl_graphql_lib.php
@@ -1,9 +1,10 @@
 <?php
     function get_curl($token, $json) {
         $curl = curl_init("https://api.github.com/graphql");
+        $args = array("Content-Type: application/json", "Content-Length: ".strlen($json), "Authorization: bearer $token");
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Length: ".strlen($json), "Authorization: bearer ".file_get_contents("oauth.token")));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $args);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_USERAGENT, "RandomAgent");
         $data = curl_exec($curl);
