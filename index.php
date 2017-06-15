8704a517634fce71bfb8484d481fb52fd685b261
Caleb Smith <karagenit@outlook.com>
2017-06-15 19:23:55 +0000

    Add Github Auth Redirect

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <a href="query.php">Search Repos</a>
+    <a href="query.php">Search Repos</a><br>
+    <a href="https://github.com/login/oauth/authorize?client_id=<?php echo file_get_contents("client_id.token"); ?>">Authorize via Github</a>
 </body>
 </html>
