48a9b490429153551bd259ae92d6e491d308a5e0
Caleb Smith <karagenit@outlook.com>
2017-06-15 19:27:03 +0000

    Cleanup Index PHP

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
+<?php
+    $client_id = file_get_contents("client_id.token");
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
     <a href="query.php">Search Repos</a><br>
-    <a href="https://github.com/login/oauth/authorize?client_id=<?php echo file_get_contents("client_id.token"); ?>">Authorize via Github</a>
+    <a href="https://github.com/login/oauth/authorize?client_id=<?php echo $client_id; ?>">Authorize via Github</a>
 </body>
 </html>
