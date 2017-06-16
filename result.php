9254f9ab2f713851481c67421c6d88e74eaf7fcf
Caleb Smith <karagenit@outlook.com>
2017-06-16 20:40:01 +0000

    Make Results Page Use Bootstrap
    Closes #10

--- result.php
+++ result.php
@@ -1,5 +1,4 @@
 <?php
-
     session_start();
     require 'lib/curl-graphql.php';
 
@@ -21,12 +20,6 @@
     $forks = json_decode(get_curl($token, $json));
     $sorted_forks = array();
 
-    echo "<pre>";
-    
-    foreach($forks->errors as $error) {
-        echo $error->message;
-    }
-    
     foreach($forks->data->repository->forks->edges as $fork) {
         $sorted_forks[$fork->node->nameWithOwner] = $fork->node->watchers->totalCount;
         $sorted_forks[$fork->node->nameWithOwner] += $fork->node->stargazers->totalCount;
@@ -35,10 +28,53 @@
     }
     
     arsort($sorted_forks);
-    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Find the most popular forks of a repository!">
+    <meta name="author" content="Caleb Smith">
+
+    <title>Github Fork Finder</title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Repo</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+<?php
     foreach($sorted_forks as $fork=>$points) {
-        echo $fork."\t".$points."\n";
+        echo "<tr>";
+        echo "<th>$fork</th>";
+        echo "<th>$points</th>";
+        echo "</tr>";
     }
+?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
+
+
+<?php
     
-    echo "</pre>";
+    foreach($forks->errors as $error) {
+        echo $error->message;
+    }
 ?>
